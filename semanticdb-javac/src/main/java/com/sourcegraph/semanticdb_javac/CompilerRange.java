823c945e5e53a55662a63c477849c40fac86a492
Ólafur Páll Geirsson <olafurpg@gmail.com>
2021-02-11 11:15:36 +0100

    WIP

---
+++ semanticdb-javac/src/main/java/com/sourcegraph/semanticdb_javac/CompilerRange.java
@@ -0,0 +1,26 @@
+package com.sourcegraph.semanticdb_javac;
+
+/**
+ * Describes how to convert a compiler position into SemanticDB Range.
+ *
+ * A Java compiler position has tree parts:
+ *
+ * <pre>
+ *     
+ * </pre>
+ */
+public enum CompilerRange {
+  FROM_START,
+  FROM_POINT,
+  FROM_POINT_PLUS_ONE;
+
+  public boolean isFromPoint() {
+    switch (this) {
+      case FROM_POINT:
+      case FROM_POINT_PLUS_ONE:
+        return true;
+      default:
+        return false;
+    }
+  }
+}
